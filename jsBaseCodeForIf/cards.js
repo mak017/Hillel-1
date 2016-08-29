2ba2a9a1a34696ae0b8b0e9c26b9e46d2adf9537
denis <makovskyy0983@gmail.com>
2016-08-29 20:40:55 +0300

    login check

--- jsBaseCodeForIf/cards.js
+++ jsBaseCodeForIf/cards.js
@@ -1,7 +1,22 @@
 
-$(document).ready(function (argument) {
-	$('#login').on('submit', function(event) {
+$(document).ready(function () {
+	$('form#login').on('submit', function(event) {
 		event.preventDefault();
-		console.log(event);
+		var login;
+		var password;
+		var CORRECT_LOGIN = 'admin';
+		var CORRECT_PASSWORD = 'SecretP@ssword';
+		
+		login = $('#login .login__input').val();
+		password = $('#login .login__password').val();
+		
+		if (login === CORRECT_LOGIN && password === CORRECT_PASSWORD) {
+			message = "You successfully authorized";
+		} else {
+			message = "Wrong login password";
+		}
+		
+		$('.login__message').text(message);
 	});
 });
+
